b9226165773de50740fb8d1c66972b43d8d0da05
Matthew Bellringer <matthew@elumin.co.uk>
2021-07-21 18:41:43 +0100

    Rearranged content settings

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
-    <nuxt-content :document="page" />
+    <h1>
+      {{ all.sitetitle }}
+    </h1>
+    <nuxt-content :document="page1" />
   </div>
 </template>
 
 <script>
+/* eslint-disable */
 export default {
   async asyncData ({ $content }) {
-    const page = await $content('hello')
+    const page = await $content('pages/index-body-1')
       .fetch()
     return {
       page
+    }
+  },
+    async asyncData ({ $content }) {
+    const all = await $content('all')
+      .fetch()
+    return {
+      all
     }
   },
   head () {
