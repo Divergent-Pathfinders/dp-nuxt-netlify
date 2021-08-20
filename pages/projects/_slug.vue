8c0014957b7ae0b16d025a428a327ec73f32e5c7
Matthew Bellringer <matthew@elumin.co.uk>
2021-08-20 16:04:46 +0100

    added projects section

---
+++ pages/projects/_slug.vue
@@ -0,0 +1,15 @@
+<template>
+  <article>
+    <nuxt-content :document="project" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+    const project = await $content('projects', params.slug).fetch()
+
+    return { project }
+  }
+}
+</script>
---
+++ pages/projects/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Projects</h1>
+    <ul>
+      <li v-for="project in projects" :key="project.title">
+        <nuxt-link to="#">
+          {{ project.title }}
+        </nuxt-link>
+      </li>
+      <div align="center">
+        <b-card
+          v-for="project in projects"
+          :key="project.title"
+          style="max-width: 100rem;"
+          align="left"
+        >
+          <nuxt-content :document="project" />
+        </b-card>
+      </div>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+    const projects = await $content('projects', params.slug).fetch()
+
+    return { projects }
+  }
+}
+</script>
